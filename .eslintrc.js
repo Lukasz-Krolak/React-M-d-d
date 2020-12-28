cb2fd857235d18fe5175c95b346d8f934f81fb8c
Łukasz Królak <lukasz.krolak@gmail.com>
2020-12-28 12:30:52 +0100

    start files

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
