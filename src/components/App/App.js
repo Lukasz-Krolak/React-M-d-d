f3988bcad463d257cd52724956864bb68a1f51dd
Łukasz Królak <lukasz.krolak@gmail.com>
2020-12-30 14:56:48 +0100

    add working route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-// import Home from '../Views/Home';
-// import MyRecipe from '../Views/MyRecipe';
-// import styles from './App.scss';
-import {BrowserRouter} from 'react-router-dom';
+import Home from '../Views/Home/Home';
+import MyRecipe from '../Views/MyRecipe/MyRecipe';
+import styles from './App.scss';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App () {
   return (
     <BrowserRouter>
       <div className='App'>
-        <p>123</p>
-        {/* <main className={styles.component}>
+        <main className={styles.component}>
           <Route exact path="/" component={Home} />
           <Route exact path="/myrecipe" component={MyRecipe} />
-        </main> */}
+        </main>
       </div>
     </BrowserRouter>
   );
--- src/components/Views/Home/Home.js
+++ src/components/Views/Home/Home.js
@@ -2,25 +2,21 @@ import React from 'react';
 //import styles from './Home.scss';
 import Board from '../../Board/Board';
 import NewRecipe from '../../NewRecipe/NewRecipe';
-import PropTypes from 'prop-types';
-import Container from '../Container/Container';
 
 class Home extends React.Component {
 
   static propTypes = {
-    title: PropTypes.node,
-    subtitle: PropTypes.node,
-    lists: PropTypes.array.isRequired,
+
   }
   
   render() {
     return (
-      <Container>
+      <div>
         <main>
           <Board/>
           <NewRecipe/> 
         </main>
-      </Container>
+      </div>
     );
   }
 }
