3f53441322b5204754cbd7a40ba0e98440e893de
Łukasz Królak <lukasz.krolak@gmail.com>
2021-01-04 01:40:26 +0100

    add views

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Boards from '../Board/Boards';
-
+import {BrowserRouter, Route} from 'react-router-dom';
+import {AnimatedSwitch} from 'react-router-transition';
+import styles from './App.scss';
+import Header from '../Header/Header';
+import MyRecipe from '../MyRecipe/MyRecipe';
 const App = () => (
-  <div className='App'>
-    <Boards/>
-  </div>
+  <BrowserRouter>
+    <div className='App'>
+      <Header/>
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className={styles.switchWrapper}
+      >
+        <Route exact path="/" component={Boards} />
+        <Route exact path="/myrecipe" component={MyRecipe} />
+      </AnimatedSwitch>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ src/components/MyRecipe/MyRecipe.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class MyRecipe extends React.Component {
+    static propTypes = {
+
+    }
+    render(){
+      return(
+        <div>
+          <main>
+            <p>dodane składniki</p>
+          </main>
+        </div>
+      );
+    }
+}
+
+export default MyRecipe;
